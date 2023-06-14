c97d8dc0de2a6b0554d6b3d02f33f95fede2a6c0
zt <960686470@qq.com>
2023-06-14 17:06:56 +0800

    updata Code

---
+++ api/src/main/java/com/wzy/api/config/RedissonConfig.java
@@ -0,0 +1,28 @@
+package com.wzy.api.config;
+
+import org.redisson.Redisson;
+import org.redisson.api.RedissonClient;
+import org.redisson.config.Config;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author YukeSeko
+ * redissen 配置
+ */
+@Configuration
+public class RedissonConfig {
+
+    @Value("${spring.redis.password}")
+    private String redisPassword;
+
+    @Bean(destroyMethod = "shutdown")
+    public RedissonClient redisson(){
+        Config config = new Config();
+        config.useSingleServer().setAddress("redis://106.13.211.93:6679");
+//        config.useSingleServer().setAddress("redis://localhost:6379");
+        config.useSingleServer().setPassword(redisPassword);
+        return Redisson.create(config);
+    }
+}
